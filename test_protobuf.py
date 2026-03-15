2604f869addc605777bfed6ffb7e3dc97b214122
Adrian Zienkiewicz <154380633+Zienek12@users.noreply.github.com>
2026-03-15 21:48:18 +0100

    Merged branch feature/proto-v1-device-state-change with tix-12 and standarize the structure also updated test_protobuf.py

--- test_protobuf.py
+++ test_protobuf.py
@@ -1,18 +1,27 @@
-import message_pb2
+from pathlib import Path
+import sys
 
-msg = message_pb2.DeviceStateChange() #nowa pusta wiadomosc
 
+PROJECT_ROOT = Path(__file__).resolve().parent
+SRC_DIR = PROJECT_ROOT / "src"
+
+if str(SRC_DIR) not in sys.path:
+	sys.path.insert(0, str(SRC_DIR))
+
+from smart_home.proto.v1 import message_pb2
+
+
+msg = message_pb2.DeviceStateChange()
 msg.device_id = 1
 msg.device_type = 2
 msg.parameters["temperature"] = "24.6"
 
-data = msg.SerializeToString() #serializacja
-
+data = msg.SerializeToString()
 print(data)
 
-received = message_pb2.DeviceStateChange() #druga pusta wiadomosc
-received.ParseFromString(data) #deserializacja
+received = message_pb2.DeviceStateChange()
+received.ParseFromString(data)
 
-print(received.device_id) 
+print(received.device_id)
 print(received.device_type)
 print(received.parameters["temperature"])
