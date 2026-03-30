bf0164499be93b9fb3f82ef3bcc872eed7a467c9
karber9 <150918920+karber9@users.noreply.github.com>
2026-03-30 12:39:38 +0200

    Add files via upload

---
+++ src/smart_home/client/models/containers.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass, field
+from typing import Dict
+from .devices import Device
+
+@dataclass
+class DeviceStorage:
+    #id urządzenia -> konkretne urządzenie
+    lamps: Dict[int, Device] = field(default_factory=dict)
+    thermometers: Dict[int, Device] = field(default_factory=dict)
+    sensors: Dict[int, Device] = field(default_factory=dict)
+    ACs: Dict[int, Device] = field(default_factory=dict) #klima
