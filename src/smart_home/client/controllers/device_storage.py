f7c52a2860848679f8e534c73dad54c7274e371b
Adrian Zienkiewicz <154380633+Zienek12@users.noreply.github.com>
2026-05-17 13:18:07 +0200

    fixed issiues

--- src/smart_home/client/controllers/device_storage.py
+++ src/smart_home/client/controllers/device_storage.py
@@ -1,5 +1,5 @@
-from .containers import DeviceStorage
-from .device import Device
+from ..models.containers import DeviceStorage
+from ..models.device import Device
 
 
 def add_device_to_storage(storage: DeviceStorage, device: Device) -> tuple[bool, str]:
