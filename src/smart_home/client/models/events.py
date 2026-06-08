c9211e2ca16a0c5d6e914262d0060b34710b822e
karber9 <150918920+karber9@users.noreply.github.com>
2026-06-08 20:03:53 +0200

    Update events.py

--- src/smart_home/client/models/events.py
+++ src/smart_home/client/models/events.py
@@ -8,4 +8,4 @@ class StorageEvent:
     timestamp: str = field(default_factory=lambda: datetime.now().isoformat())
     device_id: Optional[int] = None
     device_data: Optional[dict[str, Any]] = None 
-    new_simulated_time: Optional[str] = None
+    duration: Optional[str] = None
